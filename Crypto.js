e3f35f0fa46f39a46b9416f18d015919806bea27
Jake Nelson <couldbejake@gmail.com>
2023-08-16 21:45:45 +0100

    Update Crypto.js

--- Crypto.js
+++ Crypto.js
@@ -1,3 +1,61 @@
+// SOFTWARE LICENSE AGREEMENT FOR DIODE.JS, DIODE.WASM, AND ALL CODE ON DIODEMESSENGER.COM
+// IMPORTANT: Each user should review this license before using the software components. 
+
+// 1. DEFINITIONS
+// The "Software" refers specifically to certain components and all associated code from a specific domain.
+// This ensures clarity on what is being licensed.
+// "Software" refers to the Diode.js, Diode.wasm components, and all code retrieved from the domain diodemessenger.com, intended for use solely as part of the chat messenger application developed by Diode Messenger.
+
+// 2. LICENSE
+// This section details the legal binding of the agreement and the requirement of agreement for software use.
+// By accessing, installing, or using the Software, you acknowledge that this End User License Agreement (EULA) is a legally binding and valid contract and agree to be bound by its terms. If you do not agree to abide by the terms of this Agreement, you are not authorized to use or distribute the Software.
+
+// 3. LICENSE GRANT
+// This section specifies the conditions under which the software can be used. Restrictive licensing is often used to prevent unauthorized use or resale.
+// Diode Messenger grants you a non-exclusive, non-transferable license to install and use the Software solely as part of the chat messenger application developed by Diode Messenger. Any other use, modification, copying, reproduction, distribution, or resale of the Software, separate from the chat messenger application, is strictly prohibited.
+
+// 4. OWNERSHIP
+// This section ensures that the licensor retains all ownership rights, even when granting a license to use the software.
+// The Software is owned and copyrighted by Diode Messenger. Your license confers no title or ownership in the Software and is not a sale of any rights in the Software.
+
+// 5. COPYRIGHT
+// Explicitly points out that the software is protected by copyright and other intellectual property rights.
+// The Software contains copyrighted material, trade secrets, and other proprietary material. You shall not violate any applicable copyright, trademark, or other intellectual property laws or regulations.
+
+// 6. NO WARRANTIES
+// A common clause that explicitly states there are no guarantees with the software, limiting potential liability.
+// Diode Messenger expressly disclaims any warranty for the Software. The Software is provided 'As Is' without any express or implied warranty of any kind.
+
+// 7. LIMITATION OF LIABILITY
+// This clause is used to limit the amount and types of damages the licensor can be held responsible for.
+// In no event shall Diode Messenger or its suppliers be liable for any damages whatsoever (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or any other pecuniary loss) arising out of the use or inability to use the Software, even if Diode Messenger has been advised of the possibility of such damages.
+
+// 8. SUBJECT TO CHANGE
+// This clause indicates the software's evolving nature, potentially open-sourcing some components in the future.
+// This license and the availability of parts of the Software are subject to change. Upon the full release of the chat messenger application, certain parts of the Software may be open-sourced to promote security and provability.
+
+// 9. TERMINATION
+// This clause details how the license can be terminated if terms are breached, ensuring control over misuse.
+// This Agreement will terminate automatically if you fail to comply with the limitations described in this Agreement. On termination, you must destroy all copies of the Software.
+
+// 10. GOVERNING LAW
+// Indicates which country's laws will govern the agreement. It's vital for dispute resolution.
+// This Agreement shall be governed by the laws of the United Kingdom.
+
+// 11. ENTIRE AGREEMENT
+// Specifies that this agreement is the complete understanding between the parties, which can prevent claims of additional implied terms.
+// This is the entire agreement between you and Diode Messenger which supersedes any prior agreement or understanding, whether written or oral, relating to the subject matter of this license.
+
+// By using the software, the user agrees to all these terms.
+// By installing or using the Software, you agree to be bound by the terms of this Agreement.
+
+// The official entity behind the software and the date this license is in effect.
+// Diode Messenger
+
+// 2023-08-16
+
+// -- END OF SOFTWARE LICENSE AGREEMENT -- //
+
 function DiodeCrypto(){
   var crypto = this;
 
